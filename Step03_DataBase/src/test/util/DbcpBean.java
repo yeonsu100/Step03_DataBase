b377a5f23575c153d60832b495997f24ee62d89c
Winnie <yeonsu100@gmail.com>
2020-01-08 12:56:23 +0900

    add a comment at 'DcbpBean.java'

--- Step03_DataBase/src/test/util/DbcpBean.java
+++ Step03_DataBase/src/test/util/DbcpBean.java
@@ -14,6 +14,8 @@ public class DbcpBean {
 			Context initContext = new InitialContext();
 			Context envContext  = (Context)initContext.lookup("java:/comp/env");
 			DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+			// Connection pool에서 Connection 객체 하나 가져오기!
+			// 따라서 Connection 객체가 필요하면 new DbcpBean().getConn(); 하고 불러오면 된다.
 			conn = ds.getConnection();
 			System.out.println("Connection 얻어오기 성공!");
 		}catch(Exception e) {
